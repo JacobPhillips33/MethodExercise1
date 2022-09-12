ec5327a3a77726054f06c66a787adb186dfab72f
Jacob Phillips <jakephillips.wv@gmail.com>
2022-09-12 16:34:36 -0500

    initial commit

---
+++ MethodExercise1/Methods.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise1
+{
+    internal class Methods
+    {
+        public static void Exercise1()
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine($"Hi, {name}, what is your favorite color?");
+            var color = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine($"{color} is a great color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine($"Who wouldn't love a {animal}? What is your favorite band?");
+            var band = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine($"Would you believe me if I told you that one time I was wearing a {color} shirt when I met a {animal} named {name} " +
+                              $"at a concert by {band}?");
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1/ num2;
+        }
+
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+
+        public static int BonusAdd(params int[] bonusAdd)
+        {
+            int totalAdd = 0;
+
+            foreach(int num in bonusAdd)
+            {
+                totalAdd += num;
+            }
+
+            return totalAdd;
+        }
+    }        
+}
---
+++ MethodExercise1/Program.cs
@@ -0,0 +1,33 @@
+﻿//---------------------------Exercise 1-----------------------------------------
+
+using MethodExercise1;
+
+Methods.Exercise1();
+
+//---------------------------Exercise 2-----------------------------------------
+
+Console.WriteLine();
+Console.WriteLine("----------------------------Exercise 2-----------------------------------");
+Console.WriteLine();
+
+Console.WriteLine($"Add Method Answer: {Methods.Add(7, 12)}");
+Console.WriteLine();
+
+Console.WriteLine($"Subtract Method Answer: {Methods.Subtract(81, 33)}");
+Console.WriteLine();
+
+Console.WriteLine($"Multiply Method Answer: {Methods.Multiply(21, 37)}");
+Console.WriteLine();
+
+Console.WriteLine($"Divide Method Answer: {Methods.Divide(100, 10)}");
+Console.WriteLine();
+
+Console.WriteLine($"Modulus Method Answer: {Methods.Modulus(17, 3)}");
+Console.WriteLine();
+
+Console.WriteLine();
+Console.WriteLine("----------------------------Bonus Exercise--------------------------------");
+Console.WriteLine();
+
+Console.WriteLine($"BonusAdd Method Answer: {Methods.BonusAdd(12, 42, 33, 121)}");
+Console.WriteLine();
